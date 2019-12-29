ecfebfa5559609b4aaed2870e24b3b4a055105d2
ozzy <ozzy@cybertekschool.com>
2019-12-29 11:40:36 -0500

    EU2

---
+++ Java_Europe_EU2/src/review_sessions/week3_printStatements/PrintStatement.java
@@ -0,0 +1,25 @@
+package review_sessions.week3_printStatements;
+
+public class PrintStatement {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		
+		System.out.print("Java");
+
+		System.out.print(" 13");
+		
+		System.out.println(" Europe-eu2");
+		
+		System.out.println("SDET");
+		
+		System.out.println("123");
+		
+		System.out.println(123);
+		
+		System.out.println("He said \"hello\" ");
+
+	}
+
+}
