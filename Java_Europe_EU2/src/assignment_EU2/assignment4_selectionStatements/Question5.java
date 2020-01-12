490b51f161ad60a32c05765899fe170c1481c8c0
ozzy <ozzy@cybertekschool.com>
2020-01-12 01:52:50 +0000

    EU2

---
+++ Java_Europe_EU2/src/assignment_EU2/assignment4_selectionStatements/Question5.java
@@ -0,0 +1,21 @@
+package assignment_EU2.assignment4_selectionStatements;
+
+public class Question5 {
+
+	public static void main(String[] args) {
+		
+		int month=10;
+		int day=6;
+		int year=60;
+		
+		if((month*day)==year) {
+			System.out.println("Date is magic");
+		}else {
+			System.out.println("Date is not magic");
+		}
+		
+		
+
+	}
+
+}
